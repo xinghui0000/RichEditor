b7172065b02f64b720503a802a7a031114e943bd
lixinghui <li.xinghui@trs.com.cn>
2017-11-12 13:00:31 +0800

    init commit

---
+++ richeditor/src/main/java/jp/wasabeef/richeditor/RichEditor.java
@@ -0,0 +1,554 @@
+package jp.wasabeef.richeditor;
+
+import android.annotation.SuppressLint;
+import android.content.Context;
+import android.content.res.TypedArray;
+import android.graphics.Bitmap;
+import android.graphics.drawable.Drawable;
+import android.os.Build;
+import android.text.TextUtils;
+import android.util.AttributeSet;
+import android.util.Log;
+import android.util.Pair;
+import android.view.Gravity;
+import android.webkit.JavascriptInterface;
+import android.webkit.WebChromeClient;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+import org.w3c.dom.Text;
+
+import java.io.UnsupportedEncodingException;
+import java.net.URLDecoder;
+import java.net.URLEncoder;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Locale;
+
+/**
+ * Copyright (C) 2015 Wasabeef
+ * <p>
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * <p>
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * <p>
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+public class RichEditor extends WebView {
+    public static final String TAG = RichEditor.class.getName();
+
+    public enum Type {
+        BOLD,
+        ITALIC,
+        SUBSCRIPT,
+        SUPERSCRIPT,
+        STRIKETHROUGH,
+        UNDERLINE,
+        FONTSIZE,
+        FORECOLOR,
+        H1,
+        H2,
+        H3,
+        H4,
+        H5,
+        H6,
+        ORDEREDLIST,
+        UNORDEREDLIST,
+        JUSTIFYLEFT,
+        JUSTIFYRIGHT,
+        JUSTIFYCENTER,
+        JUSTIFYFULL;
+
+        private Object mValue = null;
+
+        public Object getValue() {
+            return mValue;
+        }
+
+        public void setValue(Object mValue) {
+            switch (this) {
+                case FONTSIZE:
+                    this.mValue = Integer.parseInt(mValue.toString().replaceAll("[\\D]", ""));
+                    break;
+                case FORECOLOR:
+                    this.mValue = "#" + mValue.toString().split("#")[1];
+                    break;
+                default:
+                    break;
+            }
+        }
+    }
+
+    public interface OnTextChangeListener {
+
+        void onTextChange(String text);
+    }
+
+    public interface OnDecorationStateListener {
+
+        void onStateChangeListener(String text, List<Type> types);
+    }
+
+    public interface AfterInitialLoadListener {
+
+        void onAfterInitialLoad(boolean isReady);
+    }
+
+    public interface OnFocusChangedListener {
+        void onBlur();
+        void onFocus();
+    }
+
+
+
+    private static final String SETUP_HTML = "file:///android_asset/editor.html";
+    private static final String CALLBACK_SCHEME = "re-callback://";
+    private static final String INTERFACE_SCHEME = "re-interface://";
+    private static final String STATE_SCHEME = "re-state://";
+    private boolean isReady = false;
+    private String mContents;
+    private OnTextChangeListener mTextChangeListener;
+    private OnDecorationStateListener mDecorationStateListener;
+    private AfterInitialLoadListener mLoadListener;
+    private OnFocusChangedListener mOnFocusChangedListener;
+
+    public RichEditor(Context context) {
+        this(context, null);
+    }
+
+    public RichEditor(Context context, AttributeSet attrs) {
+        this(context, attrs, android.R.attr.webViewStyle);
+    }
+
+    @SuppressLint("SetJavaScriptEnabled")
+    public RichEditor(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+
+        setVerticalScrollBarEnabled(false);
+        setHorizontalScrollBarEnabled(false);
+        getSettings().setJavaScriptEnabled(true);
+        setWebChromeClient(new WebChromeClient());
+        setWebViewClient(createWebviewClient());
+        loadUrl(SETUP_HTML);
+
+        applyAttributes(context, attrs);
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
+            WebView.setWebContentsDebuggingEnabled(true);
+        }
+        post(new Runnable() {
+            @Override
+            public void run() {
+                int px = getHeight();
+                setEditorHeight(px);
+            }
+        });
+
+    }
+
+    protected EditorWebViewClient createWebviewClient() {
+        return new EditorWebViewClient();
+    }
+
+    public void setOnTextChangeListener(OnTextChangeListener listener) {
+        mTextChangeListener = listener;
+    }
+
+    public void setOnDecorationChangeListener(OnDecorationStateListener listener) {
+        mDecorationStateListener = listener;
+    }
+
+    public void setOnInitialLoadListener(AfterInitialLoadListener listener) {
+        mLoadListener = listener;
+    }
+
+    public void setOnFocusChangedListener(OnFocusChangedListener mOnFocusChangedListener) {
+        this.mOnFocusChangedListener = mOnFocusChangedListener;
+    }
+
+    private void callback(String text) {
+        mContents = text.replaceFirst(CALLBACK_SCHEME, "");
+        if (mTextChangeListener != null) {
+            mTextChangeListener.onTextChange(mContents);
+        }
+    }
+
+    private void stateCheck(String text) {
+        String state = text.replaceFirst(STATE_SCHEME, "").toUpperCase(Locale.ENGLISH);
+        List<Type> types = new ArrayList<>();
+        String[] stateParts = state.split(",");
+        for (Type type : Type.values()) {
+            for (String part : stateParts) {
+                if (TextUtils.indexOf(part, type.name()) != -1) {
+                    types.add(type);
+                    type.setValue(part);
+                }
+            }
+        }
+
+
+        if (mDecorationStateListener != null) {
+            mDecorationStateListener.onStateChangeListener(state, types);
+        }
+
+        if (mOnFocusChangedListener != null) {
+            mOnFocusChangedListener.onFocus();
+        }
+    }
+
+    private void interfaceCallback(String text) {
+        String method = text.replaceFirst(INTERFACE_SCHEME, "").toLowerCase();
+        if ("onfocus".equals(method)) {
+            if (mOnFocusChangedListener != null) {
+                mOnFocusChangedListener.onFocus();
+            }
+        }
+        if ("onblur".equals(method)) {
+            if (mOnFocusChangedListener != null) {
+                mOnFocusChangedListener.onBlur();
+            }
+        }
+    }
+
+    private void applyAttributes(Context context, AttributeSet attrs) {
+        final int[] attrsArray = new int[]{
+                android.R.attr.gravity
+        };
+        TypedArray ta = context.obtainStyledAttributes(attrs, attrsArray);
+
+        int gravity = ta.getInt(0, NO_ID);
+        switch (gravity) {
+            case Gravity.LEFT:
+                exec("javascript:RE.setTextAlign(\"left\")");
+                break;
+            case Gravity.RIGHT:
+                exec("javascript:RE.setTextAlign(\"right\")");
+                break;
+            case Gravity.TOP:
+                exec("javascript:RE.setVerticalAlign(\"top\")");
+                break;
+            case Gravity.BOTTOM:
+                exec("javascript:RE.setVerticalAlign(\"bottom\")");
+                break;
+            case Gravity.CENTER_VERTICAL:
+                exec("javascript:RE.setVerticalAlign(\"middle\")");
+                break;
+            case Gravity.CENTER_HORIZONTAL:
+                exec("javascript:RE.setTextAlign(\"center\")");
+                break;
+            case Gravity.CENTER:
+                exec("javascript:RE.setVerticalAlign(\"middle\")");
+                exec("javascript:RE.setTextAlign(\"center\")");
+                break;
+        }
+
+        ta.recycle();
+    }
+
+    public void setHtml(String contents) {
+        if (contents == null) {
+            contents = "";
+        }
+        try {
+            exec("javascript:RE.setHtml('" + URLEncoder.encode(contents, "UTF-8") + "');");
+        } catch (UnsupportedEncodingException e) {
+            // No handling
+        }
+        mContents = contents;
+    }
+
+    public String getHtml() {
+        return mContents;
+    }
+
+    public void setEditorFontColor(int color) {
+        String hex = convertHexColorString(color);
+        exec("javascript:RE.setBaseTextColor('" + hex + "');");
+    }
+
+    public void setEditorFontSize(int px) {
+        exec("javascript:RE.setBaseFontSize('" + px + "px');");
+    }
+
+    @Override
+    public void setPadding(int left, int top, int right, int bottom) {
+        super.setPadding(left, top, right, bottom);
+        exec("javascript:RE.setPadding('" + left + "px', '" + top + "px', '" + right + "px', '" + bottom
+                + "px');");
+    }
+
+    @Override
+    public void setPaddingRelative(int start, int top, int end, int bottom) {
+        // still not support RTL.
+        setPadding(start, top, end, bottom);
+    }
+
+    public void setEditorBackgroundColor(int color) {
+        setBackgroundColor(color);
+    }
+
+    @Override
+    public void setBackgroundColor(int color) {
+        super.setBackgroundColor(color);
+    }
+
+    @Override
+    public void setBackgroundResource(int resid) {
+        Bitmap bitmap = Utils.decodeResource(getContext(), resid);
+        String base64 = Utils.toBase64(bitmap);
+        bitmap.recycle();
+
+        exec("javascript:RE.setBackgroundImage('url(data:image/png;base64," + base64 + ")');");
+    }
+
+    @Override
+    public void setBackground(Drawable background) {
+        Bitmap bitmap = Utils.toBitmap(background);
+        String base64 = Utils.toBase64(bitmap);
+        bitmap.recycle();
+
+        exec("javascript:RE.setBackgroundImage('url(data:image/png;base64," + base64 + ")');");
+    }
+
+    public void setBackground(String url) {
+        exec("javascript:RE.setBackgroundImage('url(" + url + ")');");
+    }
+
+    public void setEditorWidth(int px) {
+        exec("javascript:RE.setWidth('" + px + "px');");
+    }
+
+    public void setEditorHeight(int px) {
+        exec("javascript:RE.setHeight('" + px + "px');");
+    }
+
+    public void setPlaceholder(String placeholder) {
+        exec("javascript:RE.setPlaceholder('" + placeholder + "');");
+    }
+
+    public void loadCSS(String cssFile) {
+        String jsCSSImport = "(function() {" +
+                "    var head  = document.getElementsByTagName(\"head\")[0];" +
+                "    var link  = document.createElement(\"link\");" +
+                "    link.rel  = \"stylesheet\";" +
+                "    link.type = \"text/css\";" +
+                "    link.href = \"" + cssFile + "\";" +
+                "    link.media = \"all\";" +
+                "    head.appendChild(link);" +
+                "}) ();";
+        exec("javascript:" + jsCSSImport + "");
+    }
+
+    public void undo() {
+        exec("javascript:RE.undo();");
+    }
+
+    public void redo() {
+        exec("javascript:RE.redo();");
+    }
+
+    public void setBold() {
+        exec("javascript:RE.setBold();");
+    }
+
+    public void setItalic() {
+        exec("javascript:RE.setItalic();");
+    }
+
+    public void setSubscript() {
+        exec("javascript:RE.setSubscript();");
+    }
+
+    public void setSuperscript() {
+        exec("javascript:RE.setSuperscript();");
+    }
+
+    public void setStrikeThrough() {
+        exec("javascript:RE.setStrikeThrough();");
+    }
+
+    public void setUnderline() {
+        exec("javascript:RE.setUnderline();");
+    }
+
+    public void setTextColor(int color) {
+        exec("javascript:RE.prepareInsert();");
+
+        String hex = convertHexColorString(color);
+        exec("javascript:RE.setTextColor('" + hex + "');");
+    }
+
+    public void setTextBackgroundColor(int color) {
+        exec("javascript:RE.prepareInsert();");
+
+        String hex = convertHexColorString(color);
+        exec("javascript:RE.setTextBackgroundColor('" + hex + "');");
+    }
+
+    public void setFontSize(int fontSize) {
+        if (fontSize > 7 || fontSize < 1) {
+            Log.e("RichEditor", "Font size should have a value between 1-7");
+        }
+        exec("javascript:RE.setFontSize('" + fontSize + "');");
+    }
+
+    public void removeFormat() {
+        exec("javascript:RE.removeFormat();");
+    }
+
+    public void setHeading(int heading) {
+        exec("javascript:RE.setHeading('" + heading + "');");
+    }
+
+    public void setIndent() {
+        exec("javascript:RE.setIndent();");
+    }
+
+    public void setOutdent() {
+        exec("javascript:RE.setOutdent();");
+    }
+
+    public void setAlignLeft() {
+        exec("javascript:RE.setJustifyLeft();");
+    }
+
+    public void setAlignCenter() {
+        exec("javascript:RE.setJustifyCenter();");
+    }
+
+    public void setAlignFull() {
+        exec("javascript:RE.setJustifyFull();");
+    }
+
+    public void setAlignRight() {
+        exec("javascript:RE.setJustifyRight();");
+    }
+
+    public void setBlockquote() {
+        exec("javascript:RE.setBlockquote();");
+    }
+
+    public void setBullets() {
+        exec("javascript:RE.setBullets();");
+    }
+
+    public void setNumbers() {
+        exec("javascript:RE.setNumbers();");
+    }
+
+    public void insertImage(String url, String alt) {
+        exec("javascript:RE.prepareInsert();");
+        exec("javascript:RE.insertImage('" + url + "', '" + alt + "');");
+    }
+
+    public void insertImage(List<Pair<String, String>> pairs, String alt) {
+        if (pairs == null || pairs.isEmpty()) {
+            return;
+        }
+        try {
+            JSONArray jsonArray = new JSONArray();
+            for (Pair<String, String> pair : pairs) {
+                JSONObject jsonObject = new JSONObject();
+                jsonObject.put("src", pair.second);
+                jsonObject.put("uploadpic", pair.first);
+                jsonObject.put("alt", alt);
+                jsonArray.put(jsonObject);
+            }
+            exec("javascript:RE.prepareInsert();");
+            exec("javascript:RE.insertImageList('" + jsonArray.toString() + "')");
+        } catch (JSONException e) {
+
+        }
+
+    }
+
+    public void insertLink(String href, String title) {
+        exec("javascript:RE.prepareInsert();");
+        exec("javascript:RE.insertLink('" + href + "', '" + title + "');");
+    }
+
+    public void insertTodo() {
+        exec("javascript:RE.prepareInsert();");
+        exec("javascript:RE.setTodo('" + Utils.getCurrentTime() + "');");
+    }
+
+    public void focusEditor() {
+        requestFocus();
+        exec("javascript:RE.focus();");
+    }
+
+    public void clearFocusEditor() {
+        exec("javascript:RE.blurFocus();");
+    }
+
+    private String convertHexColorString(int color) {
+        return String.format("#%06X", (0xFFFFFF & color));
+    }
+
+    protected void exec(final String trigger) {
+        if (isReady) {
+            load(trigger);
+        } else {
+            postDelayed(new Runnable() {
+                @Override
+                public void run() {
+                    exec(trigger);
+                }
+            }, 100);
+        }
+    }
+
+    private void load(String trigger) {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
+            evaluateJavascript(trigger, null);
+        } else {
+            loadUrl(trigger);
+        }
+    }
+
+    protected class EditorWebViewClient extends WebViewClient {
+        @Override
+        public void onPageFinished(WebView view, String url) {
+            isReady = url.equalsIgnoreCase(SETUP_HTML);
+            if (mLoadListener != null) {
+                mLoadListener.onAfterInitialLoad(isReady);
+            }
+        }
+
+        @Override
+        public boolean shouldOverrideUrlLoading(WebView view, String url) {
+            String decode;
+            Log.d(TAG, "URL:" + url);
+            try {
+                decode = URLDecoder.decode(url, "UTF-8");
+            } catch (UnsupportedEncodingException e) {
+                // No handling
+                return false;
+            }
+
+            if (TextUtils.indexOf(url, CALLBACK_SCHEME) == 0) {
+                callback(decode);
+                exec("javascript:RE.refreshEditingItems();");
+                return true;
+            } else if (TextUtils.indexOf(url, STATE_SCHEME) == 0) {
+                stateCheck(decode);
+                return true;
+            } else if (TextUtils.indexOf(url, INTERFACE_SCHEME) == 0) {
+                interfaceCallback(decode);
+                return true;
+            }
+
+            return super.shouldOverrideUrlLoading(view, url);
+        }
+    }
+}
